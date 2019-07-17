9ff6f6352be62f5a26781566ee48e63693ceb95d
Alex Corn <mckay@doctormckay.com>
2019-07-17 11:57:41 -0400

    Use whoami to determine what user we are

--- app/init.js
+++ app/init.js
@@ -2,12 +2,14 @@
 // It will check which setup tasks still need to be run and run them, if needed.
 // Then it will run the archive task.
 
+const exec = require('child_process').execSync;
 const FS = require('fs');
 
 const Logging = require('./components/logging');
 
-if (process.env.USER != 'root') {
-	Logging.fatalSetupError(`TeslaPi init script needs to be run as root; ran as ${process.env.USER}`); // exits
+let currentUser = exec('whoami').toString('utf8').trim();
+if (currentUser != 'root') {
+	Logging.fatalSetupError(`TeslaPi init script needs to be run as root; ran as ${currentUser}`); // exits
 }
 
 require('./setup/00-run');
