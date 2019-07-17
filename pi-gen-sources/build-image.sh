e63661dd472563e0cf264e013be8fb415649e548
Alex Corn <mckay@doctormckay.com>
2019-07-17 03:55:33 -0400

    Why do 1 and l look so similar

--- pi-gen-sources/build-image.sh
+++ pi-gen-sources/build-image.sh
@@ -20,9 +20,9 @@ unzip master.zip
 cp -R node-teslapi-master/pi-gen-sources/00-teslapi-tweaks ../stage7
 cp -R node-teslapi-master/app ../stage7/00-teslapi-tweaks/files
 cp -R node-teslapi-master/bin ../stage7/00-teslapi-tweaks/files
-curl -L https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-armv6l.tar.xz > node-v10.16.0-linux-armv61.tar.xz
-tar -xf node-v10.16.0-linux-armv61.tar.xz
-mv node-v10.16.0-linux-armv61/bin/node ../stage7/00-teslapi-tweaks/files/node
+curl -L https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-armv6l.tar.xz > node-v10.16.0-linux-armv6l.tar.xz
+tar -xf node-v10.16.0-linux-armv6l.tar.xz
+mv node-v10.16.0-linux-armv6l/bin/node ../stage7/00-teslapi-tweaks/files/node
 cd ..
 rm -rf temp
 chmod +x stage7/00-teslapi-tweaks/00-run.sh
