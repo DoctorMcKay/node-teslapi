cca014af78b8cd5cf4efd8f32832e9f21b0d4306
Alex Corn <mckay@doctormckay.com>
2019-07-18 03:35:12 -0400

    Added ability to override isHostReachable output

--- app/components/hosts.js
+++ app/components/hosts.js
@@ -1,3 +1,5 @@
+const FS = require('fs').promises;
+
 const {exec} = require('./exec');
 
 /**
@@ -6,6 +8,16 @@ const {exec} = require('./exec');
  * @returns {Promise<boolean>}
  */
 exports.isHostReachable = async function(host) {
+	// Check if we have an override file
+	try {
+		let file = await FS.readFile('/mnt/mutable/override_host_reachable.txt');
+		if (file) {
+			return file.slice(0, 1).toString('ascii') == '1';
+		}
+	} catch (ex) {
+		// file probably doesn't exist; that's fine
+	}
+
 	let pingResponse = await exec(`ping -c 3 ${host}`);
 	let match = pingResponse.match(/(\d+) received/);
 	return !!(match && match[1] > 0);
